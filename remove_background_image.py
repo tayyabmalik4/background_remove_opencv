79d100599d75bde326b40822f577c9f07aaa2c21
tayyabmalik4 <mtayyabmalik99@gmail.com>
2021-12-05 20:54:17 +0500

    modify

--- remove_background_image.py
+++ remove_background_image.py
@@ -5,10 +5,13 @@ import os
 
 segmentor = SelfiSegmentation()
 
-path = 'image/7.jpg'
+path = 'image/8.jpg'
 img= cv2.imread(path,-1)
 resize = cv2.resize(img,(600,400))
-backremove=segmentor.removeBG(resize,(255,255,255),threshold=0.95)
-cv2.imshow('remove background color',backremove)
+backremove=segmentor.removeBG(resize,(255,255,255),threshold=0.01)
+imgstacked = cvzone.stackImages([resize,backremove],2,1)
+cv2.imshow('remove background color',imgstacked)
+cv2.imwrite("image/converted_image.jpg",imgstacked)
+# cv2.imshow('remove background color',backremove)
 key = cv2.waitKey(0)
 cv2.destroyAllWindows()
